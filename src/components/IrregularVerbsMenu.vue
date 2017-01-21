dc4f9a3b587c06f074e5d2acb045a2f44b938fc0
Jaime Escolar <jaime.escolar@gmail.com>
2017-01-21 13:11:41 +0100

    Removed test Menu option 1 and 2

--- src/components/IrregularVerbsMenu.vue
+++ src/components/IrregularVerbsMenu.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <span class="text">Test Length:</span>
+    <!--
     <button class='button is-primary is-small' @click='changeQuizLength(1)'>1</button>
     <button class='button is-primary is-small' @click='changeQuizLength(2)'>2</button>
+    -->
     <button class='button is-primary is-small' @click='changeQuizLength(10)'>10</button>
     <button class='button is-primary is-small' @click='changeQuizLength(50)'>50</button>
     <button class='button is-primary is-small' @click='changeQuizLength(0)'>All</button>
