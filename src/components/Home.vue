afbb69e740478ec05c24a299fff2dab032f07ac0
Jaime Escolar <jaime.escolar@gmail.com>
2017-02-22 19:39:45 +0100

    removed my name

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,12 +8,6 @@
         <ul class="menu-list">
           <li><router-link :to="{ name: 'irregularVerbs' }">Irregular Verbs</router-link></li>
         </ul>
-        <p class="menu-label">
-          Developer
-        </p>
-        <ul class="menu-list">
-          <li><a href="https://github.com/Jaimeer" target="_blanck">Jaime Escolar</a></li>
-        </ul>
       </aside>
   </div>
 </template>
